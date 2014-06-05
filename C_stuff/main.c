58839ae54131d1bc4884d6f3d4b6f98a6dcf7b23
EvgenyKarkan <evgenykarkan@gmail.com>
2014-06-05 17:24:22 +0300

    Refactored

--- C_stuff/main.c
+++ C_stuff/main.c
@@ -14,13 +14,9 @@ int main(int argc, char *argv[])
 
 void printSizeOfTypes()
 {
-    int _array[] = {10, 12, 13, 14, 20, 42};
-    char name[] = "Chuck";
-    char full_name[] = {
-        'C', 'h', 'u',
-         'c', 'k', '.', 'N',
-         'o', 'r', 'i', 's', '\0'
-    };
+    int _array[]     = {10, 12, 13, 14, 20, 42};
+    char name[]      = "Chuck";
+    char full_name[] = {'C', 'h', 'u', 'c', 'k', '.', 'N', 'o', 'r', 'i', 's', '\0'};
 
     printf("The size of an int: %u bytes \n", sizeof(int));
     printf("The size of an float: %u bytes \n", sizeof(float));
@@ -30,10 +26,8 @@ void printSizeOfTypes()
     printf("The size of an long long int: %u bytes \n", sizeof(long long int));
 
     printf("The size of an array of ints: %u bytes \n", sizeof(_array));
-    printf("The size of an array of chars: %u bytes \n", sizeof(name));
+    printf("The size of an array from string: %u bytes \n", sizeof(name));
     printf("The size of an array of chars: %u bytes \n", sizeof(full_name));
-
-
 
 }
 
