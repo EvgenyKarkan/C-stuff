622debc6a58a339d7d4667dfccbb4c81506f1654
EvgenyKarkan <evgenykarkan@gmail.com>
2014-06-06 14:32:57 +0300

    Added more C types

--- C_stuff/main.c
+++ C_stuff/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <inttypes.h>
+#include <stdint.h>
+
 
 
 void printSizeOfTypes();
@@ -27,15 +29,38 @@ void printSizeOfTypes()
 
     printf("The size of an int: %u bytes \n", sizeof(int));
     printf("The size of an unsigned int: %u bytes \n", sizeof(unsigned int));
-    printf("The size of an int8_t: %u bytes \n", sizeof(int8_t));
-    printf("The size of an int16_t: %u bytes \n", sizeof(int16_t));
-    printf("The size of an int32_t: %u bytes \n", sizeof(int32_t));
+
     printf("The size of an long int: %u bytes \n", sizeof(long int));
     printf("The size of an long long int: %u bytes \n", sizeof(long long int));
     printf("The size of an array of short int: %u bytes \n", sizeof(short int));
+    printf("The size of an unsigned long int: %u bytes \n", sizeof(unsigned long int));
+
+    printf("The size of an uint8_t: %u bytes \n", sizeof(uint8_t));
+    printf("The size of an uint16_t: %u bytes \n", sizeof(uint16_t));
+    printf("The size of an uint32_t: %u bytes \n", sizeof(uint32_t));
+    printf("The size of an uint64_t: %u bytes \n", sizeof(uint64_t));
+
+    printf("The size of an int8_t: %u bytes \n", sizeof(int8_t));
+    printf("The size of an int16_t: %u bytes \n", sizeof(int16_t));
+    printf("The size of an int32_t: %u bytes \n", sizeof(int32_t));
+    printf("The size of an int64_t: %u bytes \n", sizeof(int64_t));
+
+    printf("The size of an int_least8_t: %u bytes \n", sizeof(int_least8_t));
+    printf("The size of an int_least16_t: %u bytes \n", sizeof(int_least16_t));
+    printf("The size of an int_least32_t: %u bytes \n", sizeof(int_least32_t));
+    printf("The size of an int_least64_t: %u bytes \n", sizeof(int_least64_t));
+
+    printf("The size of an int_fast8_t: %u bytes \n", sizeof(int_fast8_t));
+    printf("The size of an int_fast16_t: %u bytes \n", sizeof(int_fast16_t));
+    printf("The size of an int_fast32_t: %u bytes \n", sizeof(int_fast32_t));
+    printf("The size of an int_fast64_t: %u bytes \n", sizeof(int_fast64_t));
+
+    printf("The size of an intptr_t: %u bytes \n", sizeof(intptr_t));
+    printf("The size of an uintptr_t: %u bytes \n", sizeof(uintptr_t));
 
     printf("The size of an float: %u bytes \n", sizeof(float));
-    printf("The size of an double: %u bytrs \n", sizeof(double));
+    printf("The size of an double: %u bytes \n", sizeof(double));
+    printf("The size of an long double: %u bytes \n", sizeof(long double));
     printf("The size of an BOOL: %u bytes \n", sizeof(_Bool));
 
     printf("The size of an array of ints: %u bytes \n", sizeof(_array));
