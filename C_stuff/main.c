ce27bf2cc0c2ef22ba2fd6cf2aa873eda5dbb586
Evgeny Karkan <evgenykarkan@gmail.com>
2014-07-04 13:37:56 +0300

    i386 arch update

--- C_stuff/main.c
+++ C_stuff/main.c
@@ -49,66 +49,66 @@ int main(int argc, char *argv[])
 
     enum dummyEnum Jazz = 4;
 
-    printf("The size of an enum %u bytes \n", sizeof(Jazz));
+    printf("The size of an enum %lu bytes \n", sizeof(Jazz));
 
-    printf("The size of an int: %u bytes \n", sizeof(int));
-    printf("The size of an unsigned int: %u bytes \n", sizeof(unsigned int));
+    printf("The size of an int: %lu bytes \n", sizeof(int));
+    printf("The size of an unsigned int: %lu bytes \n", sizeof(unsigned int));
 
-    printf("The size of an long int: %u bytes \n", sizeof(long int));
-    printf("The size of an long long int: %u bytes \n", sizeof(long long int));
-    printf("The size of an array of short int: %u bytes \n", sizeof(short int));
-    printf("The size of an unsigned long int: %u bytes \n", sizeof(unsigned long int));
+    printf("The size of an long int: %lu bytes \n", sizeof(long int));
+    printf("The size of an long long int: %lu bytes \n", sizeof(long long int));
+    printf("The size of an array of short int: %lu bytes \n", sizeof(short int));
+    printf("The size of an unsigned long int: %lu bytes \n", sizeof(unsigned long int));
 
-    printf("The size of an uint8_t: %u bytes \n", sizeof(uint8_t));
-    printf("The size of an uint16_t: %u bytes \n", sizeof(uint16_t));
-    printf("The size of an uint32_t: %u bytes \n", sizeof(uint32_t));
-    printf("The size of an uint64_t: %u bytes \n", sizeof(uint64_t));
+    printf("The size of an uint8_t: %lu bytes \n", sizeof(uint8_t));
+    printf("The size of an uint16_t: %lu bytes \n", sizeof(uint16_t));
+    printf("The size of an uint32_t: %lu bytes \n", sizeof(uint32_t));
+    printf("The size of an uint64_t: %lu bytes \n", sizeof(uint64_t));
 
-    printf("The size of an int8_t: %u bytes \n", sizeof(int8_t));
-    printf("The size of an int16_t: %u bytes \n", sizeof(int16_t));
-    printf("The size of an int32_t: %u bytes \n", sizeof(int32_t));
-    printf("The size of an int64_t: %u bytes \n", sizeof(int64_t));
+    printf("The size of an int8_t: %lu bytes \n", sizeof(int8_t));
+    printf("The size of an int16_t: %lu bytes \n", sizeof(int16_t));
+    printf("The size of an int32_t: %lu bytes \n", sizeof(int32_t));
+    printf("The size of an int64_t: %lu bytes \n", sizeof(int64_t));
 
-    printf("The size of an int_least8_t: %u bytes \n", sizeof(int_least8_t));
-    printf("The size of an int_least16_t: %u bytes \n", sizeof(int_least16_t));
-    printf("The size of an int_least32_t: %u bytes \n", sizeof(int_least32_t));
-    printf("The size of an int_least64_t: %u bytes \n", sizeof(int_least64_t));
+    printf("The size of an int_least8_t: %lu bytes \n", sizeof(int_least8_t));
+    printf("The size of an int_least16_t: %lu bytes \n", sizeof(int_least16_t));
+    printf("The size of an int_least32_t: %lu bytes \n", sizeof(int_least32_t));
+    printf("The size of an int_least64_t: %lu bytes \n", sizeof(int_least64_t));
 
-    printf("The size of an int_fast8_t: %u bytes \n", sizeof(int_fast8_t));
-    printf("The size of an int_fast16_t: %u bytes \n", sizeof(int_fast16_t));
-    printf("The size of an int_fast32_t: %u bytes \n", sizeof(int_fast32_t));
-    printf("The size of an int_fast64_t: %u bytes \n", sizeof(int_fast64_t));
+    printf("The size of an int_fast8_t: %lu bytes \n", sizeof(int_fast8_t));
+    printf("The size of an int_fast16_t: %lu bytes \n", sizeof(int_fast16_t));
+    printf("The size of an int_fast32_t: %lu bytes \n", sizeof(int_fast32_t));
+    printf("The size of an int_fast64_t: %lu bytes \n", sizeof(int_fast64_t));
 
-    printf("The size of an intptr_t: %u bytes \n", sizeof(intptr_t));
-    printf("The size of an uintptr_t: %u bytes \n", sizeof(uintptr_t));
-    printf("The size of an intmax_t: %u bytes \n", sizeof(intmax_t));
-    printf("The size of an uintmax_t: %u bytes \n", sizeof(uintmax_t));
+    printf("The size of an intptr_t: %lu bytes \n", sizeof(intptr_t));
+    printf("The size of an uintptr_t: %lu bytes \n", sizeof(uintptr_t));
+    printf("The size of an intmax_t: %lu bytes \n", sizeof(intmax_t));
+    printf("The size of an uintmax_t: %lu bytes \n", sizeof(uintmax_t));
 
-    printf("The size of an float: %u bytes \n", sizeof(float));
-    printf("The size of an double: %u bytes \n", sizeof(double));
-    printf("The size of an long double: %u bytes \n", sizeof(long double));
-    printf("The size of an BOOL: %u bytes \n", sizeof(_Bool));
+    printf("The size of an float: %lu bytes \n", sizeof(float));
+    printf("The size of an double: %lu bytes \n", sizeof(double));
+    printf("The size of an long double: %lu bytes \n", sizeof(long double));
+    printf("The size of an BOOL: %lu bytes \n", sizeof(_Bool));
 
-    printf("The size of an NULL: %u bytes \n", sizeof(NULL));
+    printf("The size of an NULL: %lu bytes \n", sizeof(NULL));
 
-    printf("The size of an ptrdiff_t: %u bytes \n", sizeof(ptrdiff_t));
+    printf("The size of an ptrdiff_t: %lu bytes \n", sizeof(ptrdiff_t));
 
-    printf("The size of an char: %u bytes \n", sizeof(char));
-    printf("The size of an unsigned char: %u bytes \n", sizeof(unsigned char));
-    printf("The size of an wchar_t: %u bytes \n", sizeof(wchar_t));
+    printf("The size of an char: %lu bytes \n", sizeof(char));
+    printf("The size of an unsigned char: %lu bytes \n", sizeof(unsigned char));
+    printf("The size of an wchar_t: %lu bytes \n", sizeof(wchar_t));
 
-    printf("The size of an float _Complex: %u bytes \n", sizeof(float _Complex));
-    printf("The size of an double _Complex: %u bytes \n", sizeof(double _Complex));
+    printf("The size of an float _Complex: %lu bytes \n", sizeof(float _Complex));
+    printf("The size of an double _Complex: %lu bytes \n", sizeof(double _Complex));
 
-    printf("The size of an  size_t: %u bytes \n", sizeof(size_t));
+    printf("The size of an  size_t: %lu bytes \n", sizeof(size_t));
 
-    printf("The size of an void: %u bytes \n", sizeof(void));
+    printf("The size of an void: %lu bytes \n", sizeof(void));
 
-    printf("The size of an array of ints: %u bytes \n", sizeof(_array));
-    printf("The size of an array from string: %u bytes \n", sizeof(name));
-    printf("The size of an array of chars: %u bytes \n", sizeof(full_name));
+    printf("The size of an array of ints: %lu bytes \n", sizeof(_array));
+    printf("The size of an array from string: %lu bytes \n", sizeof(name));
+    printf("The size of an array of chars: %lu bytes \n", sizeof(full_name));
 
-    printf("The size of an struct: %u bytes \n", sizeof(theFoo));
+    printf("The size of an struct: %lu bytes \n", sizeof(theFoo));
 }
 
  void printLimits()
@@ -129,7 +129,7 @@ int main(int argc, char *argv[])
 
     printf("Max value of short: %d\n", SHRT_MAX);
     printf("Min value of short: %d\n", SHRT_MIN);
-    printf("Max value of unsigned short: %hu\n", USHRT_MAX);
+    printf("Max value of unsigned short: %d\n", USHRT_MAX);
 
     printf("Max value of long: %ld\n", LONG_MAX);
     printf("Min value of long: %ld\n", LONG_MIN);
